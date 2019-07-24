6104945582202c4c09d7141dd92e7df13b844d49
Aleksander Chmiel <aleksander.chmiel@emc.com>
2019-07-24 14:17:47 +0100

    Eureka lab update permissions

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.allocations;
 
 import org.springframework.web.client.RestOperations;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private Map<Long, ProjectInfo> map = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        storeProjectInMemory(project, projectId);
+
+        return project;
+    }
+
+    private void storeProjectInMemory(ProjectInfo project, long id){
+        map.put(id, project);
+    }
+
+    private ProjectInfo getProjectFromCache(long projectId) {
+        return map.get(projectId);
     }
 }

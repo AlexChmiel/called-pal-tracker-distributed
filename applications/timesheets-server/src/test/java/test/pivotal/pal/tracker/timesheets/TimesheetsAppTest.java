123656aa773e7b05df5c7f3e9939fc8fe527baeb
Aleksander Chmiel <aleksander.chmiel@emc.com>
2019-07-30 14:12:51 +0100

    Security lab

--- applications/timesheets-server/src/test/java/test/pivotal/pal/tracker/timesheets/TimesheetsAppTest.java
+++ applications/timesheets-server/src/test/java/test/pivotal/pal/tracker/timesheets/TimesheetsAppTest.java
@@ -1,22 +1,20 @@
 package test.pivotal.pal.tracker.timesheets;
 
 import io.pivotal.pal.tracker.timesheets.App;
+
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimesheetsAppTest {
 
-    @Autowired
-    private RestTemplate restTemplate;
 
     @Test
     public void embedded() {
         App.main(new String[]{});
 
-        String response = restTemplate.getForObject("http://localhost:8181/time-entries?userId=0", String.class);
+        String response = new RestTemplate().getForObject("http://localhost:8181/time-entries?userId=0", String.class);
 
         assertThat(response).isEqualTo("[]");
     }
